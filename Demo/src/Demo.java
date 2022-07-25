d1d1748462e49884887a58722ef2ba7ffc0a5611
FieBee <phuonglf98@gmail.com>
2022-07-25 17:54:01 +0700

    Add TH-CopyLargeFiles

---
+++ Demo/src/Demo.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Demo {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
